8d3a0449893229e67f5e45b0ab42a87eeee04bb1
Philip Turk <pturk@westerndataanalytics.com>
2020-12-16 14:15:56 -0500

    Update Ex01.R

--- Ex01.R
+++ Ex01.R
@@ -1,4 +1,4 @@
-## 2020/02/28 Phil's R script
+## 2020/12/18 Phil's R script
 
 x <- rnorm(100)
 mean(x) 
@@ -6,4 +6,4 @@ y <- rnorm(100)
 
 plot(x, y) ## Symbols look too basic, Phil.  Don't you have any sense of style?
 # plot(x, y, pch = 19) ## Still not happy with black circles.  我不高兴。
-plot(x, y, pch = 17) ## Heck yeah, Casey!
+plot(x, y, pch = 17) ## Heck yeah, Jing!
